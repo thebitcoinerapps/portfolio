750d392769359b9d6afc56655dd28fad96810add
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-11 18:32:25 +0200

    basic layout added using react mdl

---
+++ src/components/aboutme.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Aboutme extends React.Component{
+
+    render(){
+        return(
+            <div>About me</div>
+        );
+    };
+
+}
+export default Aboutme;
---
+++ src/components/cv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Cv extends React.Component{
+
+    render(){
+        return(
+            <div>Cv</div>
+        );
+    };
+
+}
+export default Cv;

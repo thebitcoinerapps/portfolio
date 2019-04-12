cb8291518033c2ba0e3c989de4bf7164c560633f
theBitCoiner <thebitcoinerapps@gmail.com>
2019-04-12 13:31:21 +0200

    colors added

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ class Contact extends React.Component{
 
     render(){
         return(
-            <div>Contact</div>
+            <div className="container"></div>
         );
     };
 
--- src/components/cv.js
+++ src/components/cv.js
@@ -4,7 +4,7 @@ class Cv extends React.Component{
 
     render(){
         return(
-            <div>Cv</div>
+            <div class="container"></div>
         );
     };
 
